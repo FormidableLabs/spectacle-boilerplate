156d482b45ba32de8b33fed01b8acef0b5621621
Ken Wheeler <ken_wheeler@me.com>
2015-12-07 08:59:12 -0500

    initial

---
+++ README.markdown
@@ -0,0 +1,2 @@
+#Spectacle Boilerplate
+
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
+import Presentation from "./presentation";
+
+render(<Presentation/>, document.getElementById("root"));

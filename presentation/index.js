ff1246fe480cbce19a8c9dda5aea8b7fa888a5bf
Ken Wheeler <ken_wheeler@me.com>
2015-12-07 22:59:59 -0500

    1.0.0 prep

--- presentation/index.js
+++ presentation/index.js
@@ -1,6 +1,8 @@
+// Import React
 import React from "react";
 import { render } from "react-dom";
 
+// Import Spectacle Core tags
 import {
   Appear,
   BlockQuote,
@@ -21,12 +23,19 @@ import {
   Text
 } from "spectacle";
 
+// Import image preloader util
 import preloader from "spectacle/lib/utils/preloader";
 
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Import custom component
 import Interactive from "../assets/interactive";
 
+// Require CSS
 require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
+
 
 const images = {
   city: require("../assets/city.jpg"),
@@ -35,10 +44,16 @@ const images = {
   markdown: require("../assets/markdown.png")
 };
 
+preloader(images);
+
+const theme = createTheme({
+  primary: "#ff4081"
+});
+
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Spectacle>
+      <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
           <Slide transition={["zoom"]} bgColor="primary">
             <Heading size={1} fit caps lineHeight={1} textColor="black">
@@ -57,7 +72,7 @@ export default class Presentation extends React.Component {
           </Slide>
           <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
             <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={1} fit textColor="primary" textFont="secondary">
+            <Heading size={2} caps fit textColor="primary" textFont="primary">
               Wait what?
             </Heading>
           </Slide>

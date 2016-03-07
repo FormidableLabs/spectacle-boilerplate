c3a87650fdc0b93670d380917e6bb1fcb3f19721
Alan Foster <alan@alanfoster.me>
2016-03-06 21:25:21 -0500

    Allow configurable port

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ var config = require("./webpack.config");
 var app = express();
 var compiler = webpack(config);
 
+var serverPort = process.env.PORT || 3000;
+
 app.use(require("webpack-dev-middleware")(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
@@ -19,11 +21,11 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, "localhost", function (err) {
+app.listen(serverPort, "localhost", function (err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log("Listening at http://localhost:3000");
+  console.log("Listening at http://localhost:" + serverPort);
 });

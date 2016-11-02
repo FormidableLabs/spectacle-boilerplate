5910826eb08f744cfb170c81267776563e86ce92
Jonathan Cooper <Jon@Jonathans-MacBook-Pro-5.local>
2016-11-02 15:50:16 -0400

    Adding basic elements to the example slides

--- presentation/index.js
+++ presentation/index.js
@@ -48,8 +48,8 @@ preloader(images);
 const theme = createTheme({
   primary: "white",
   secondary: "#1F2022",
-  tertiary: "#2fc7f5",
-  quartenary: "#2fc7f5",
+  tertiary: "#03A9FC",
+  quartenary: "#CECECE",
 },{
   primary: "Montserrat",
   secondary: "Helvetica"
@@ -68,13 +68,29 @@ export default class Presentation extends React.Component {
               open the presentation/index.js file to get started
             </Text>
           </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <Heading size={2} caps textColor="tertiary">slide title</Heading>
+          <Slide transition={["fade"]} bgColor="tertiary">
+            <Heading size={6} textColor="primary" caps>Typography</Heading>
+            <Heading size={1} textColor="secondary">Heading 1</Heading>
+            <Heading size={2} textColor="secondary">Heading 2</Heading>
+            <Heading size={3} textColor="secondary">Heading 3</Heading>
+            <Heading size={4} textColor="secondary">Heading 4</Heading>
+            <Heading size={5} textColor="secondary">Heading 5</Heading>
+            <Text    size={6} textColor="secondary">Standard text</Text>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+            <Heading size={6} textColor="secondary" caps>Standard List</Heading>
             <List>
-              <ListItem>Item 1...</ListItem>
-              <ListItem>Item 2...</ListItem>
-              <ListItem>Item 3...</ListItem>
+              <ListItem>Item 1</ListItem>
+              <ListItem>Item 2</ListItem>
+              <ListItem>Item 3</ListItem>
+              <ListItem>Item 4</ListItem>
             </List>
+          </Slide>
+          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+            <BlockQuote>
+              <Quote>Example Quote</Quote>
+              <Cite>Author</Cite>
+            </BlockQuote>
           </Slide>
         </Deck>
       </Spectacle>
